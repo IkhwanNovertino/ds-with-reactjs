2ddd065e36ba9a21c26dff0f4f2a5ab1b3aa42f2
ikhwanNovertino <m.ikhwan.pilliang@gmail.com>
2023-03-11 21:50:54 +0800

    init project and setup worksheet

---
+++ src/stories/Badge/example/Default.stories.tsx
@@ -0,0 +1,21 @@
+// Badge.stories.ts|tsx
+
+import React from 'react';
+
+import { ComponentMeta } from '@storybook/react';
+
+import Badge from '../../../component/Badge';
+
+export default {
+  /* 👇 The title prop is optional.
+  * See https://storybook.js.org/docs/react/configure/overview#configure-story-loading
+  * to learn how to generate automatic titles
+  */
+  title: 'Component/Badge/Example',
+  component: Badge,
+} as ComponentMeta<typeof Badge>;
+
+export const Success = (args: any) => <Badge {...args}>Success</Badge>;
+Success.args = {
+  state: "success",
+}; 
